9c581ad94e6436193e72e7a4005e1c6d9c905638
Teanna Dixon <teannadixon09@gmail.com>
2025-11-26 12:40:12 -0500

    Completed landing page

--- scripts.js
+++ scripts.js
@@ -1,1 +1,19 @@
 /* Add your JavaScript to this file */
+ document.addEventListener('DOMContentLoaded', function() {
+    const newsletform = document.querySelector('.newsletter form');
+    const emailin = document.getElementById('email');
+    const mess = document.querySelector('.newsletter .message');
+
+    newsletform.addEventListener('submit', function(e) {
+        e.preventDefault();
+        const email = emailin.value.trim();
+
+        if (email === '') {
+            mess.textContent = "Please enter a valid email address.";
+        } else {
+            mess.textContent = `Thank you! Your email "${email}" has been added to our  mailing list!`;
+            emailin.value = '';
+        }
+    });
+});
+
